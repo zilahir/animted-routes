6c7fb16653294c6cbf1c22f6d81ac6da89256e34
Richard Zilahi <richard.zilahi@salesframe.com>
2020-03-29 23:07:41 +0300

    added duration prop

--- src/components/MountTransition/index.tsx
+++ src/components/MountTransition/index.tsx
@@ -4,17 +4,22 @@ import { motion } from 'framer-motion'
 type Props = {
 	slide?: number
 	slideUp?: number
+	duration?: number,
 }
 
 export const MountTransition: FC<Props> = ({
 	children,
 	slide = 0,
 	slideUp = 0,
+	duration = 0.2,
 }) => (
 	<motion.div
 		exit={{ opacity: 0, x: slide, y: slideUp }}
 		initial={{ opacity: 0, x: slide, y: slideUp }}
 		animate={{ opacity: 1, x: 0, y: 0 }}
+		transition={{
+			duration
+		}}
 	>
 		{children}
 	</motion.div>
